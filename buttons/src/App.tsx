12cbc47d4ba3afb5b081e0f3ec2867e5e63d11d7
Rafinhaa <rafinhaa@me.com>
2024-10-10 19:04:15 -0300

    feat: implement button

--- buttons/src/App.tsx
+++ buttons/src/App.tsx
@@ -1,5 +1,13 @@
+import Button from "./components/Button";
+import styles from "./app.module.css";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className={styles.app}>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+    </div>
+  );
 }
 
 export default App;
