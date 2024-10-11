03903de99fc888d95c2595578ef63f60a4006fa4
Rafinhaa <rafinhaa@me.com>
2024-10-11 14:57:05 -0300

    feat: auth context

--- container/src/routes/index.tsx
+++ container/src/routes/index.tsx
@@ -1,17 +1,40 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 
 import Layout from "@/components/Layout"
+import { useAuth } from "@/context/auth"
 import Clients from "@/pages/Clients"
 import ClientsSelected from "@/pages/ClientsSelected"
 import Home from "@/pages/Home"
 
+type PrivateRouteProps = {
+  isLogged: boolean
+  children: JSX.Element
+}
+
+const PrivateRoute = ({ isLogged, children }: PrivateRouteProps) => {
+  if (!isLogged) {
+    return <Navigate to="/" replace />
+  }
+  return children
+}
+
 export const Router = () => {
+  const { username } = useAuth()
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/" element={<Layout />}>
-        <Route path="/clients" element={<Clients />} />
-        <Route path="/clients-selected" element={<ClientsSelected />} />
+
+      <Route
+        path="/"
+        element={
+          <PrivateRoute isLogged={!!username}>
+            <Layout />
+          </PrivateRoute>
+        }
+      >
+        <Route path="clients" element={<Clients />} />
+        <Route path="clients-selected" element={<ClientsSelected />} />
       </Route>
     </Routes>
   )
