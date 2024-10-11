0e558c15435869391d4930e992677623de029466
Rafinhaa <rafinhaa@me.com>
2024-10-11 16:45:04 -0300

    refactor: modal

--- container/src/components/Modal/index.tsx
+++ container/src/components/Modal/index.tsx
@@ -1,17 +1,31 @@
+import { forwardRef, useImperativeHandle, useState } from "react"
 import { MdClose } from "react-icons/md"
 
 import styles from "./styles.module.css"
 
 export type ModalProps = {
   title: string
-  isOpen: boolean
-
-  onClose: () => void
 
   children?: React.ReactNode
 }
 
-const Modal = ({ title, isOpen, onClose, children }: ModalProps) => {
+export type ModalRef = {
+  open: () => void
+  close: () => void
+}
+
+const Modal = forwardRef<ModalRef, ModalProps>(({ title, children }, ref) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  useImperativeHandle(ref, () => ({
+    open: () => {
+      setIsOpen(true)
+    },
+    close: () => {
+      setIsOpen(false)
+    },
+  }))
+
   return (
     isOpen && (
       <div>
@@ -20,7 +34,7 @@ const Modal = ({ title, isOpen, onClose, children }: ModalProps) => {
         <div className={styles.modal}>
           <header>
             <h3>{title}</h3>
-            <button onClick={onClose}>
+            <button onClick={() => setIsOpen(false)}>
               <MdClose size={24} />
             </button>
           </header>
@@ -30,6 +44,6 @@ const Modal = ({ title, isOpen, onClose, children }: ModalProps) => {
       </div>
     )
   )
-}
+})
 
 export default Modal
